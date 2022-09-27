1a02aca94c41c4cedf7600839cf9f1261dcadfaf
Suhasshenoy <66239522+Suhasshenoy@users.noreply.github.com>
2022-09-27 18:37:09 +0530

    modified env and gitignore

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static(__dirname + "/cb"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/",(req,res)=>{
     res.render("home");
