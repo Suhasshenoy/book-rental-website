5331f6d5274145f28ab2b44ed9192af46df21699
Suhasshenoy <66239522+Suhasshenoy@users.noreply.github.com>
2022-09-27 11:45:21 +0530

    Added gitignore and updated ports

--- app.js
+++ app.js
@@ -191,6 +191,6 @@ app.post("/deleteBook",(req,res)=>{
 })
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT||3000, function () {
     console.log("Server started on port 3000");
   });
